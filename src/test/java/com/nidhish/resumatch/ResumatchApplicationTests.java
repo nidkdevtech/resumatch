ac6cd3143209a21b1d39e93b635b3b5a4a445a8c
Nidhish Kumar Singh <nidhishkumar986@gmail.com>
2026-06-21 17:16:01 +0530

    initial commit: Spring Boot project setup for ResuMatch

---
+++ src/test/java/com/nidhish/resumatch/ResumatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidhish.resumatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
